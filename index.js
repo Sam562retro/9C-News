ddfa5e210c30152989be1eac779ad2095a3e6b7c
Soham Gupta <gsoham562@gmail.com>
2021-06-12 12:33:03 +0000

    Done Most Of The Project Made all frontend Just a few links to be fixed here and there Have to add proper news

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const compression = require('compression');
 const helmet = require('helmet');
+const path = require('path');
+const details = require('./newsDetails');
+
+app.use(helmet.dnsPrefetchControl({allow : false}));
+app.use(helmet.expectCt());
+app.use(helmet.frameguard({action : 'deny'}));
+app.use(helmet.hidePoweredBy({setTo : 'Django/1.2.1 SVN-13336'}));
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy({policy : 'same-origin'}));
+app.use(helmet.xssFilter());
 
 app.use(compression({level: 9, threshold: 0}));
 app.set('view engine', 'ejs')
+
+app.use(express.static(path.resolve(__dirname, './media')));
 
 app.get('/', (req, res) => {
 	res.render('home');
@@ -15,7 +30,12 @@ app.get('/news', (req, res) => {
 })
 
 app.get('/news/:type', (req, res) => {
-	res.render('showNews');
+	const type = req.params.type;
+	if(type === 'international'){
+		res.render('showNews', details.internationalNews);
+	}else{
+		res.redirect('/not-found');
+	}
 })
 
 app.get('/main-video', (req, res) => {
@@ -25,5 +45,9 @@ app.get('/main-video', (req, res) => {
 app.get('/not-found', (req, res) => {
 	res.render('not-found');
 })
+
+app.get('*', (req, res) => {
+  res.status(404).redirect('/not-found');
+});
 
 app.listen(8000);
---
+++ newsDetails.js
@@ -0,0 +1,18 @@
+exports.internationalNews = {
+	head : "International News",
+	contentBy : "Example Name",
+	spokenBy : "Example Name",
+	vidLink : '/videos/vid1.mp4',
+	scripts : [
+		{
+			scriptHead : "Test Script",
+			scriptBody : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt porttitor enim sed cursus. Fusce eget venenatis tellus. Fusce mattis mauris sem, nec mattis lacus faucibus vitae. Etiam eros nunc, faucibus ac odio non, placerat finibus augue. Integer et libero sodales, interdum mauris ut, cursus felis. Quisque dapibus est nibh, eget finibus ligula faucibus at. Fusce eu orci suscipit, elementum dolor a, interdum magna. Nam lorem mauris",
+			scriptId : "s1"
+		}, 
+		{
+			scriptHead : "Test Script 2",
+			scriptBody : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt porttitor enim sed cursus. Fusce eget venenatis tellus. Fusce mattis mauris sem, nec mattis lacus faucibus vitae. Etiam eros nunc, faucibus ac odio non, placerat finibus augue. Integer et libero sodales, interdum mauris ut, cursus felis. Quisque dapibus est nibh, eget finibus ligula faucibus at. Fusce eu orci suscipit, elementum dolor a, interdum magna. Nam lorem mauris",
+			scriptId : "s2"
+		}
+	]
+}
